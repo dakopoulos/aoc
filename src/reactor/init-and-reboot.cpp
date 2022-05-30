7acc364a2fa79de06a28a63437179494661f98d3
Dimitris Dakopoulos <dakopoulos@gmail.com>
2022-05-30 23:01:10 +0100

    Add comments

--- src/reactor/init-and-reboot.cpp
+++ src/reactor/init-and-reboot.cpp
@@ -11,18 +11,20 @@ int main(int argc, char* argv[])
     // create reactor
     Reactor reactor({{-50, 50}, {-50, 50}, {-50, 50}});
 
-    // read init sequence
+    // args
     if(argc != 2 && argc != 3) {
         std::cerr << "incorrect arguments; usage: ./reboot-reactor "
             " <init-sequence-file> [<boot-sequence-file]" << std::endl;
     }
-    
+
+    // init
     std::cout << "read & apply init sequence..." << std::endl;
     apply_boot_sequence(reactor,
         boost::lexical_cast<std::string>(argv[1]),
         Reactor::Mode::INIT);
     std::cout << "...done" << std::endl;
 
+    // boot
     if(argc == 3) {
         std::cout << "read & apply boot sequence..." << std::endl;
         apply_boot_sequence(reactor,
@@ -31,7 +33,7 @@ int main(int argc, char* argv[])
         std::cout << "...done" << std::endl;
     }
 
-    // prints
+    // print
     std::cout << "total active cores: " << reactor.active_cores() << std::endl;
 
     return 0;
